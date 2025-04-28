e2d05f92ed05af8ff02000858d98df71f925779e
Greemyn <matic.riznar1@gmail.com>
2025-04-28 11:12:13 +0200

    bookbot update

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from stats import get_num_words
+import sys
+
+arg_lenght = len(sys.argv)
+if arg_lenght != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+from stats import get_book_text 
 
--- stats.py
+++ stats.py
@@ -1,13 +1,15 @@
+import sys
 
-def get_book_text():
-    with open("books/frankenstein.txt") as book:
+
+def get_book_text(X):
+    with open(X) as book:
         file_contents = book.read()    
     return file_contents
     
 def get_num_words(X):       
     words_in_book = X.split()
     num_words =len(words_in_book)
-    return f"{num_words} words found in the document"
+    return num_words
 
 def char_counter(X):
     letter_count ={}
@@ -19,13 +21,25 @@ def char_counter(X):
                 letter_count[lcase_letter] += 1
             else:
                 letter_count[lcase_letter] = 1
-    print(letter_count)
-
-            
+    return letter_count
 
 
+def char_print(X):
+    for key, value in X.items():
+        print(f"{key}: {value}") 
+    
 
-book = get_book_text()
+
+book = get_book_text(sys.argv[1])
 word_number = get_num_words(book)
-char_counter(book)
+char_dict = char_counter(book)
+
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {sys.argv[1]}...")
+print("----------- Word Count ----------")
+print(f"Found {word_number} total words")
+print("--------- Character Count -------")
+char_print(char_dict)
+print("============= END ===============")
 
